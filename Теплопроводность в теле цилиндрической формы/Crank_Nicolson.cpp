ea2d1a41f06ae80828009c5e058701998e2ce3b8
Daniil <lopatin.daniil2014@mail.ru>
2022-10-29 02:43:02 +0300

    Main

--- Теплопроводность в теле цилиндрической формы/Crank_Nicolson.cpp
+++ Теплопроводность в теле цилиндрической формы/Crank_Nicolson.cpp
@@ -10,10 +10,8 @@ double q   [SIZE_X];
 
 double T0(double x) 
 {
-    if (x < b) return 350;
-    else return 330;
-
-    // return 350;
+    if (x < b) return 310;
+    else return 300;
 }
 
 void fill()
@@ -58,42 +56,39 @@ void Crank_Nicolson()
         double a_ = -k[SIZE_X-1]/h + alpha/2;
         double b_ =  k[SIZE_X-1]/h + alpha/2;
         double f_ = alpha*TT; 
-        double g_ = -sigma/pow(2, 4); //g_ = 0;
+        double g_ = -sigma; //g_ = 0;
 
-        double prev_temp = 100;
-        int counter = 0;
-        // while (counter < 30) { 
-        //     prev_temp = T_CN[m+1][SIZE_X-1];
-        //     T_CN[m+1][SIZE_X-1] = (-B[SIZE_X-1]*a_ + f_ + g_*pow(T_CN[m+1][SIZE_X-1] + T_CN[m+1][SIZE_X-2], 4)) / (A[SIZE_X-1]*a_ + b_);
-        //     T_CN[m+1][SIZE_X-2] = A[SIZE_X-1]*prev_temp + B[SIZE_X-1];
-        //     counter++;
-        //     // printf("m = %d, %lf\n", m+1,  T_CN[m+1][SIZE_X-1]);
-        //     // getchar();
-        // }        
-        while (abs(prev_temp - T_CN[m+1][SIZE_X-1]) > 0.0001) { 
+        double prev_temp = 100;        
+        while (abs(prev_temp - T_CN[m+1][SIZE_X-1]) > 1e-8) { 
             prev_temp = T_CN[m+1][SIZE_X-1];
-            T_CN[m+1][SIZE_X-1] = (-B[SIZE_X-1]*a_ + f_ + g_*pow(T_CN[m+1][SIZE_X-1] + T_CN[m+1][SIZE_X-2], 4)) / (A[SIZE_X-1]*a_ + b_);
-            T_CN[m+1][SIZE_X-2] = A[SIZE_X-1]*prev_temp + B[SIZE_X-1];
-            //printf("m = %d, %lf\n", m+1,  T_CN[m+1][SIZE_X-1]);
-            //getchar();
+            T_CN[m+1][SIZE_X-1] = (-B[SIZE_X-1]*a_ + f_ + g_*pow((T_CN[m+1][SIZE_X-1] + A[SIZE_X-1]*T_CN[m+1][SIZE_X-1] + B[SIZE_X-1])/2, 4)) / (A[SIZE_X-1]*a_ + b_);
         }
 
         for(int n = SIZE_X-1; n >= 1; --n)
-            T_CN[m+1][n-1] = A[n]*T_CN[m+1][n] + B[n];       
+            T_CN[m+1][n-1] = A[n]*T_CN[m+1][n] + B[n];
+
+        // if (m == SIZE_T-2) {
+        //     printf("i = 1, test_solution = %lf\n", T_CN[m+1][0] - T_CN[m+1][1]);
+        //     for(int i = 2; i < SIZE_X; ++i) {
+        //         double a_ = -(k[i-1]*(i-2)) / (2*h*h*(i-1.5));
+        //         double b_ = (c[i-1]*ro[i-1]) / t + (k[i]*(i-1) + k[i-1]*(i-2)) / (2*h*h*(i-1.5));
+        //         double c_ = -(k[i]*(i-1)) / (2*h*h*(i-1.5));
+        //         double f_ = -a_ * T_CN[m][i-2] + (2*c[i-1]*ro[i-1]/t - b_) * T_CN[m][i-1] -c_ * T_CN[m][i] + (q[i-1] + q[i-1]) / 2;
+        //         double test_solution = a_*T_CN[m+1][i-2]+b_*T_CN[m+1][i-1]+c_*T_CN[m+1][i]-f_;
+        //         printf("i = %d, test_solution = %lf\n", i, test_solution);
+        //     } 
+        //     double a_ = -k[SIZE_X-1]/h + alpha/2;
+        //     double b_ =  k[SIZE_X-1]/h + alpha/2;
+        //     double f_ = alpha*TT; 
+        //     double g_ = -sigma; //g_ = 0;
+        //     printf("i = %d, test_solution = %lf\n", SIZE_X, a_*T_CN[m+1][SIZE_X-2]+b_*T_CN[m+1][SIZE_X-1]-f_-g_*pow((T_CN[m+1][SIZE_X-2]+T_CN[m+1][SIZE_X-1])/2, 4));
+        // }     
     }    
 }
 
-// int time_moments[5] = {
-//     (int)(0    * SIZE_T / 5), 
-//     (int)(0.05 * SIZE_T / 5),
-//     (int)(0.3  * SIZE_T / 5),
-//     (int)(1.   * SIZE_T / 5),
-//     (int)(4.5  * SIZE_T / 5),
-// };
-
 int time_moments[5] = {
     (int)(0    * SIZE_T / 5), 
-    (int)(0.625 * SIZE_T / 5),
+    (int)(0.625 * SIZE_T / 5), 
     (int)(1.25  * SIZE_T / 5),
     (int)(2.5   * SIZE_T / 5),
     (int)(5  * SIZE_T / 5 - 1),
